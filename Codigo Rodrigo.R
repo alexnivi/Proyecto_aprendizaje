1c008894af26ad69344d8c7c42ee33d6ba3fe493
rflorianov <rflorianov@gmail.com>
2018-10-15 15:27:29 -0500

    Actualización R

--- Codigo Rodrigo.R
+++ Codigo Rodrigo.R
@@ -3,3 +3,10 @@
 library(tidyverse)
 library(keras)
 
+# Normalizamos pixeles a [0,1] (los pixeles originalmente vienen entre 0 y 255)
+#entrena <- image_data_generator(rescale = 1/255)
+#valida <- image_data_generator(rescale = 1/255)
+
+3+2
+x<-1:10
+sum(x)
